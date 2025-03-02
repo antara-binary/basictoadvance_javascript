90e63bc6a2783149d8a3aa9194fd3a7356d0db42
antara-binary <debroyantara7@gmail.com>
2025-03-02 05:56:27 +0000

    datatype covert done

---
+++ js_basics/datatypes_convert.js
@@ -0,0 +1,61 @@
+let score1="33"
+let score2="33abc"
+let score3=null
+let score4=undefined
+
+
+// console.log(typeof score)//number
+// console.log(typeof (score))//number
+
+// let valueInNumber1=Number(score1)
+// console.log(typeof valueInNumber1)//number
+// console.log(valueInNumber1)//33
+
+// let valueInNumber2=Number(score2)
+// console.log(typeof valueInNumber2)//number
+// console.log(valueInNumber2)//NaN
+
+// let valueInNumber3=Number(score3)
+// console.log(typeof valueInNumber3)//number
+// console.log(valueInNumber3)//0
+
+// let valueInNumber4=Number(score4)
+// console.log(typeof valueInNumber4)//number
+// console.log(valueInNumber4)//NaN
+
+//"33" =>33
+//"33abc" =>NaN
+//true=>1 false=>0
+
+// let isLoggedin=1;
+// let boolloggoed= Boolean(isLoggedin);
+// console.log(typeof boolloggoed)//boolean
+// console.log(boolloggoed)//true
+
+
+// let isLoggedinspace="";
+
+// let boolloggoed= Boolean(isLoggedinspace);
+// console.log(typeof isLoggedinspace)
+// console.log(isLoggedinspace)//string
+
+//1=>true 0=>false
+//""=>string
+//"hitesh"
+
+let somenumber=33
+let stringNumber=String(somenumber);
+console.log(typeof stringNumber)//string
+console.log(stringNumber)//33
+
+
+// let somenumber1=33
+// let stringNumber1=String(somenumber1);
+// console.log(stringNumber1)//33
+
+
+
+
+
+
+

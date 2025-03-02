389b5fdd025e2dec829c14d51af49a3bf3af24ec
antara-binary <debroyantara7@gmail.com>
2025-03-02 05:55:42 +0000

    datatypes config done

---
+++ js_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)->we are using nodejs not browser
+
+let name="antara"//string
+let age=18//number
+let islogged=true;//boolean
+
+
+//number-> 2^53
+//bigint-> 
+//string->""
+//boolean->true/false
+//null=> this is a standalone value(empty)
+//undefined=>value not defined
+//symbol=>unique
+
+
+
+//object
+console.log(typeof "antara")//string
+console.log(typeof 18)//number
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+

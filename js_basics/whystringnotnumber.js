db7b3cd17a4488cb8127de0fa572c214453d3453
antara-binary <debroyantara7@gmail.com>
2025-03-02 05:56:56 +0000

    string not num

---
+++ js_basics/whystringnotnumber.js
@@ -0,0 +1,30 @@
+// let value=3
+// let negvalue=-value
+// console.log(typeof(negvalue))//number
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+//console.log(2**4)
+
+// let str1="antara"
+// let str2=" debroy"
+// console.log(str1+str2)//antara debroy
+
+// console.log( 1+ "2" )//12
+// console.log("1"+2) //12
+// console.log("1"+2+2) //122
+// console.log(1+"2"+"2") //122
+// console.log(1+2+"2") //32
+// console.log(true);//true
+// console.log(+true);//1
+// console.log(+"");//0
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// console.table([num1,num2,num3])
+
+let gameCounter=100
+console.log(gameCounter++)//100
+console.log(++gameCounter)//102
